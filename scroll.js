4e6a2be9d5e7f968e84c9fb2ee3b6c180a6c4903
Jesse Zhong <jessewzhong@gmail.com>
2019-03-09 06:12:44 -0500

    time

--- scroll.js
+++ scroll.js
@@ -2,6 +2,6 @@ for (let i = 1; i < 6; i++) {
     $("#1" + i).click(function() {
         $('html, body').animate({
             scrollTop: $("#" + i).offset().top
-        }, 1000);
+        }, 1500);
     });
 }
