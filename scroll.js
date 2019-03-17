042f829f6766ab4e636f004f6560a63c0fad8634
Jesse Zhong <jessewzhong@gmail.com>
2019-03-17 15:08:14 -0400

    scrolling in memoies

--- scroll.js
+++ scroll.js
@@ -5,3 +5,20 @@ for (let i = 1; i < 6; i++) {
         }, 1500);
     });
 }
+
+for (let i = 0; i < 5; i++) {
+    if (i == 0) {
+        $("#2" + i).click(function() {
+            $('html, body').animate({
+                scrollTop: $("#" + i).offset().top - document.getElementsByClassName("top")[0].offsetHeight
+            }, 1500);
+        });
+    }
+    else {
+        $("#2" + i).click(function() {
+            $('html, body').animate({
+                scrollTop: $("#" + i).offset().top
+            }, 1500);
+        });
+    }
+}
