2884579d6d52d43aff607083e086bd378dfd0a16
Jesse Zhong <jessewzhong@gmail.com>
2019-03-26 23:14:09 -0400

    fixed scrolling to first frame

--- scroll.js
+++ scroll.js
@@ -7,18 +7,9 @@ for (let i = 1; i < 6; i++) {
 }
 
 for (let i = 0; i < 5; i++) {
-    if (i == 0) {
-        $("#2" + i).click(function() {
-            $('html, body').animate({
-                scrollTop: $("#" + i).offset().top - document.getElementsByClassName("top")[0].offsetHeight
-            }, 1500);
-        });
-    }
-    else {
-        $("#2" + i).click(function() {
-            $('html, body').animate({
-                scrollTop: $("#" + i).offset().top
-            }, 1500);
-        });
-    }
+    $("#2" + i).click(function() {
+        $('html, body').animate({
+            scrollTop: $("#" + i).offset().top
+        }, 1500);
+    });
 }
